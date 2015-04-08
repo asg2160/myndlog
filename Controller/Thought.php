36ade38b0f9ba866b69e2fdfc9532564931c9055
Abhishek Gandhi <asg2160@gmail.com>
2015-04-08 05:20:51 -0400

    Thought controller and views removed

---
+++ Controller/Thought.php
@@ -0,0 +1,23 @@
+<?php
+class ThoughtController extends Controller {
+	function load($args) {
+		if($args['get']['tid']) {
+			
+			$thoughtID = ($args['get']['tid']);
+			$thought = new Thought($thoughtID);
+			
+			$user = new User($thought->userID);
+			$args['user_name'] = $user->userName;
+			
+			if(!$thought->visible) loadUrl('Home');
+			
+			$args['thoughtIDs'] = array('0'=>array('ID'=>$thoughtID));
+			$args['publicPage'] = getURL('Page',array('uid'=>$user->ID));
+			$args['userID'] = $user->ID;
+			
+			$this->view($args,'View/Thought/Thought.php');
+			
+		}
+	}
+}
+?>
---
+++ View/Thought/Thought.php
@@ -0,0 +1,6 @@
+<div id="article_page">
+	<?php
+	$isArticle = true;
+	include_once('View/Summary/Summary.php');
+	?>
+</div>
