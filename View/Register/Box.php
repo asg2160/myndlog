0e2011a74d5d75163d0ae1ccfaf5a538a4414e23
Abhishek Gandhi <asg2160@gmail.com>
2015-04-12 00:05:28 -0400

    mobile site view fixes

--- View/Register/Box.php
+++ View/Register/Box.php
@@ -1,35 +1,31 @@
-<div id="register_wrapper">
-	<span class="close">X</span>
-	<div id="register_box" class="article">
-		<?php if($error == 'register_unique'): ?>
-			<span class="register_error">Email's already taken!</span>
-		<?php endif; ?>
-		
-		<?php if($isMashup) { ?>
-		<a class="login">Login</a>
-		<?php } else { ?>
-			<a class="login" href="<?php echo getURL('SignIn',array('tag'=>$args['selectedTagName'])); ?>">Login</a>
-		<?php } ?>
-		
-		<form id="register" method="post" enctype="multipart/form-data" action="<?php echo getURL('Register',array('tag'=>$args['selectedTagName'])); ?>">
-			<label for="email">Email</label>
-			<input type="text" name="email" id="email">
-			
-			<label for="password">Password</label>
-			<input type="password" name="password" id="password">
-			
-			<label for="confirm_password">Confirm Password</label>
-			<input type="password" name="confirm_password" id="confirm_password">
-			
-			<label for="user_name">User Name</label>
-			<input type="text" name="user_name" id="user_name">
-			
-			<input type="hidden" name="submit" value="1">
-			<input class="submit" type="submit" value="Register">
-		</form>
-	</div>
+<div id="register_box" class="article">
+	<?php if($error == 'register_unique'): ?>
+		<span class="register_error">Email's already taken!</span>
+	<?php endif; ?>
 	
-	<?php
-		include_once('View/Register/Copy.php');
-	?>
+	<?php if($isMashup) { ?>
+	<a class="login">Login</a>
+	<?php } else { ?>
+		<a class="login" href="<?php echo getURL('SignIn',array('tag'=>$args['selectedTagName'])); ?>">Login</a>
+	<?php } ?>
+	
+	<form id="register" method="post" enctype="multipart/form-data" action="<?php echo getURL('Register',array('tag'=>$args['selectedTagName'])); ?>">
+		<label for="email">Email</label>
+		<input type="text" name="email" id="email">
+		
+		<label for="password">Password</label>
+		<input type="password" name="password" id="password">
+		
+		<label for="confirm_password">Confirm Password</label>
+		<input type="password" name="confirm_password" id="confirm_password">
+		
+		<label for="user_name">User Name</label>
+		<input type="text" name="user_name" id="user_name">
+		
+		<input type="hidden" name="submit" value="1">
+		<input class="submit" type="submit" value="Register">
+	</form>
 </div>
+<?php
+	include_once('View/Register/Copy.php');
+?>
--- View/SignIn/Box.php
+++ View/SignIn/Box.php
@@ -1,31 +1,27 @@
-<div id="sign_in_wrapper">
-	<span class="close">X</span>
-	<div id="sign_in_box" class="article">
-		<?php if($error == 'sign_in_invalid'): ?>
-			<span class="sign_in_error">Invalid Email or Password!</span>
-		<?php endif; ?>
+<div id="sign_in_box" class="article">
+	<?php if($error == 'sign_in_invalid'): ?>
+		<span class="sign_in_error">Invalid Email or Password!</span>
+	<?php endif; ?>
+
+	<form id="sign_in" method="post" action="<?php echo getURL('SignIn',array('tag'=>$args['selectedTagName'])); ?>">
+		<label for="email">Email</label>
+		<input type="text" name="email" value="<?php echo $args['rmEmail']; ?>">
+		<br>
+		<label for="password">Password</label>
+		<input type="password" name="password" value="<?php echo $args['rmPassword']; ?>">
+		<br>
+		<input type="checkbox" name="remember_me" checked="checked">
+		<label name="remember_me" for="remember_me" class="remember_me">remember me on this device</label>
+		<br>
+		<input class="submit" type="submit" name="sign_in" value="Sign In">
+	</form>
 	
-		<form id="sign_in" method="post" action="<?php echo getURL('SignIn',array('tag'=>$args['selectedTagName'])); ?>">
-			<label for="email">Email</label>
-			<input type="text" name="email" value="<?php echo $args['rmEmail']; ?>">
-			<br>
-			<label for="password">Password</label>
-			<input type="password" name="password" value="<?php echo $args['rmPassword']; ?>">
-			<br>
-			<input type="checkbox" name="remember_me" checked="checked">
-			<label name="remember_me" for="remember_me" class="remember_me">remember me on this device</label>
-			<br>
-			<input class="submit" type="submit" name="sign_in" value="Sign In">
-		</form>
-		
-		<?php if($isMashup) { ?>
-			<a class="sign_up">New User? Sign Up!</a>
-		<?php } else { ?>
-			<a class="sign_up" href="<?php echo getURL('Register',array('tag'=>$args['selectedTagName'])); ?>">New User? Sign Up!</a>
-		<?php } ?>
-	</div>
-	
-	<?php
-		include_once('View/SignIn/Copy.php');
-	?>
+	<?php if($isMashup) { ?>
+		<a class="sign_up">New User? Sign Up!</a>
+	<?php } else { ?>
+		<a class="sign_up" href="<?php echo getURL('Register',array('tag'=>$args['selectedTagName'])); ?>">New User? Sign Up!</a>
+	<?php } ?>
 </div>
+<?php
+	include_once('View/SignIn/Copy.php');
+?>
