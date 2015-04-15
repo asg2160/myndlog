e46af8690dfcc6b8e7ab098a9fa3e71ad55a6b4e
Abhishek Gandhi <asg2160@gmail.com>
2015-04-15 19:19:58 -0400

    updated min css and js files

--- Globals.php
+++ Globals.php
@@ -4,5 +4,5 @@ $GLOBALS['Page'] = "";
 $GLOBALS['ThoughtsPerQuery'] = 50;
 $GLOBALS['DefaultPage'] = 'SignIn';
 $GLOBALS['isDev'] = ($_SERVER['HTTP_HOST'] == 'localhost' || $_SERVER['SERVER_NAME'] == 'localhost');
-$GLOBALS['version'] = 3;
+$GLOBALS['version'] = 4;
 ?>
