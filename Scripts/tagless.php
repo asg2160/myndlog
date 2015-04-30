69925cf157a7bd0eaafc195c081dfb885c23ea51
Abhishek Gandhi <asg2160@gmail.com>
2015-04-30 18:12:02 -0400

    update tagless.php script

--- Scripts/tagless.php
+++ Scripts/tagless.php
@@ -1,7 +1,23 @@
 <?php
+die();
+
+// create tag if it does not already exist
+$tagObj = new Tag();
+$tagID = $tagObj->add('tagless');
+	
 $query = "SELECT ID FROM Thought where (SELECT COUNT(ID) FROM ThoughtTag WHERE ThoughtTag.ThoughtID = Thought.ID) = 0";
 $results = DB::Query($query);
+	
 foreach($results as $result) {
-	echo $result['ID']."<br>";
+	
+	if(!$tagID) continue;
+	
+	// create thought tag link if it does not already exist
+	$thoughtTagObj = new ThoughtTag();
+	$thoughtTagObj->add($result['ID'],$tagID);
+
+	echo "<br> ThoughtID : ".$result['ID']." --- TagID : ".$tagID." --- ThoughtTagID : ".$thoughtTagObj->ID;
 }
+
+die();
 ?>
